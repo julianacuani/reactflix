f570ba7422e04bdb0737c08d9efcd1a0db2760bd
Juliana <juliana.271@hotmail.com>
2020-07-28 16:51:07 -0300

    Resultado da segunda aula da imersao

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,8 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
+      <p> <h3> Por Juliana </h3> </p>
+
       <p>
         Orgulhosamente criado durante a
         {' '}
--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
-    <div style={{ background: "#F1CDD7" }}>
+    <div style={{ background: "#000000" }}>
       <Menu />
 
       <BannerMain
@@ -46,4 +46,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
