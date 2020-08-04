09a6f2b0f859d516732b482e82ecb8105c5388f7
Juliana <juliana.271@hotmail.com>
2020-08-04 17:00:10 -0300

    teste na versão do vercel

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACK = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://aluraflix.herokuapp.com';
+  : 'https://heartfix.herokuapp.com';
 
 export default {
   URL_BACK,
